d91f2a650d04d94da7ed21fa871b7aa8e8a11bd7
Ethan <ethanokamura3@gmail.com>
2025-07-01 12:36:13 -0700

    updated colors

--- frontend/roomate_expense_tracker/packages/app_ui/lib/src/colors.dart
+++ frontend/roomate_expense_tracker/packages/app_ui/lib/src/colors.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 abstract class CustomColors {
   // universal accent
   static const Color accent = Color(0xff59B3BC);
-  static const Color lightAccent = Color(0xff6cb6bd);
+  static const Color lightAccent = Color(0xff58c7d1);
 
   // dark mode
   static const Color darkBackground = Color(0xff10151a);
@@ -17,9 +17,9 @@ abstract class CustomColors {
   static const Color inverseDarkTextColor = Color(0xff151b24);
 
   // light mode
-  static const Color lightBackground = Color(0xfffdfdfa);
-  static const Color lightSurface = Color(0xfff5f5f5);
-  static const Color lightPrimary = Color(0xffe5e8ea);
+  static const Color lightBackground = Color(0xfff5f5f5);
+  static const Color lightSurface = Color(0xffe5e8ea);
+  static const Color lightPrimary = Color(0xfffdfdfa);
   static const Color lightTextColor = Color(0xff46494b);
   static const Color lightSubtextColor = Color(0xff7f8b90);
   static const Color lightHintTextColor = Color(0xff8f8f8f);
