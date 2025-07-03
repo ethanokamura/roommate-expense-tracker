56dc42f54537979ae6091471e08b1b9282eec359
Ethan <ethanokamura3@gmail.com>
2025-07-03 13:32:15 -0700

    removed unnecessary packages

--- frontend/roomate_expense_tracker/packages/app_ui/lib/app_ui.dart
+++ frontend/roomate_expense_tracker/packages/app_ui/lib/app_ui.dart
@@ -5,7 +5,6 @@ export 'package:animations/animations.dart';
 export 'package:auto_size_text/auto_size_text.dart';
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:flutter/material.dart';
-export 'package:webview_flutter/webview_flutter.dart';
 export 'package:google_fonts/google_fonts.dart';
 export 'package:intl/intl.dart' hide TextDirection;
 export 'package:fl_chart/fl_chart.dart';
