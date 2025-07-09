b5f5c3f088ed3573aea1eae0366930e278e6f793
rm89090000 <riddhimundhra05@gmail.com>
2025-07-09 12:15:35 -0700

    Update server.js

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,36 +1,16 @@
-// server.js using socket.io in JavaScript
-
+// server.js
 require("dotenv").config();
 
-const http = require("http");
-const { Server: SocketIOServer } = require("socket.io");
-const { PrismaClient } = require("@prisma/client");
 const app = require("./app");
-
-const prisma = new PrismaClient();
-const server = http.createServer(app);
-
-const socketOptions = {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  }
-};
-
-const io = new SocketIOServer(server, socketOptions);
-
-
-io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected:", socket.id);
-  });
-});
 
 const PORT = process.env.SERVER_PORT || 3001;
 
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-server.on("close", async () => {});
+server.on("close", async () => {
+});
+
+module.exports = { app, server };
+
