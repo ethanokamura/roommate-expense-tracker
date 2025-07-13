b371d82cd848cb78b9e435c515a176072636e83f
Ethan <ethanokamura3@gmail.com>
2025-07-13 10:44:49 -0700

    updated imports

--- frontend/roommate_expense_tracker/lib/features/expenses/widgets/expense_popup.dart
+++ frontend/roommate_expense_tracker/lib/features/expenses/widgets/expense_popup.dart
@@ -1,8 +1,8 @@
 import 'package:app_core/app_core.dart';
 import 'package:app_ui/app_ui.dart';
 import 'package:expenses_repository/expenses_repository.dart';
-import 'package:roommate_expense_tracker/features/expenses/widgets/cards/expense_splits_card.dart';
-import 'package:roommate_expense_tracker/features/expenses/widgets/category_data.dart';
+import 'package:roommate_expense_tracker/features/expenses/widgets/widgets.dart';
+import 'package:roommate_expense_tracker/features/expenses/page_data/page_data.dart';
 
 Future<dynamic> expensePopUp({
   required BuildContext context,
