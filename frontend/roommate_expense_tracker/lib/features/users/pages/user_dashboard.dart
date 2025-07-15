b1ff3c6c5587401c15aeb668a1cd78aa18478b74
nsagat <nsagat35@gmail.com>
2025-07-15 15:04:47 -0700

    added initial buttons

--- frontend/roommate_expense_tracker/lib/features/users/pages/user_dashboard.dart
+++ frontend/roommate_expense_tracker/lib/features/users/pages/user_dashboard.dart
@@ -5,9 +5,41 @@ class UserDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: CustomText(
-        text: 'User Dashboard',
-        style: AppTextStyles.title,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CustomText(
+            text: 'User Dashboard',
+            style: AppTextStyles.title,
+          ),
+          //Nickname:
+          CustomText(
+            text: 'Nickname: Temp',//Pull from the database. And then display it here.
+            //users can eedit their nickname.
+            style: AppTextStyles.primary,
+          ),
+          //Houses:
+          CustomText(
+            text: 'Houses: Temp',//Pull from the database. And then display it here.
+            //use the backend join query to get the houses the user is part of.
+            style: AppTextStyles.primary,
+          ),
+          //Expense History Button
+          CustomButton(
+            text: 'Expense History',
+            onTap: null,
+          ),
+          //Logout Button
+          CustomButton(
+            text: 'Logout',
+            onTap: null,
+          ),
+          //Delete Account Button
+          CustomButton(
+            text: 'Delete Account',
+            onTap: null,
+          ),
+        ],
       ),
     );
   }
