c532d7d9e8f5e3d09ed840c6a86802eec33b0736
Ethan <ethanokamura3@gmail.com>
2025-07-16 15:10:10 -0700

    removed unused stuff

--- backend/node/app.js
+++ backend/node/app.js
@@ -6,47 +6,42 @@ const { ExpenseSplitsRouter } = require("./src/features/expense-splits/router");
 const { ExpensesRouter } = require("./src/features/expenses/router");
 const { HouseMembersRouter } = require("./src/features/house-members/router");
 const { HousesRouter } = require("./src/features/houses/router");
-const { ReceiptsRouter } = require("./src/features/receipts/router");
-const { RecurringExpensesRouter } = require("./src/features/recurring-expenses/router");
 const { UserRouter } = require("./src/features/users/router");
 const { UserHousesRouter } = require("./src/features/user-houses/router");
 const { authenticateJWT } = require("./src/features/auth/middleware");
 
-
 const app = express();
 
 function isOriginAllowed(origin) {
-	return origin && /^https?:\/\/localhost(:\d+)?$/.test(origin);
+  return origin && /^https?:\/\/localhost(:\d+)?$/.test(origin);
 }
 
 const corsOpts = {
-	origin: function (org, cb) {
-		if (!org || isOriginAllowed(org)) {
-			cb(null, true);
-		} else {
-			cb(new Error("Cors not allowed"));
-		}
-	},
-	credentials: true,
+  origin: function (org, cb) {
+    if (!org || isOriginAllowed(org)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Cors not allowed"));
+    }
+  },
+  credentials: true,
 };
 
 app.use(cors(corsOpts));
 app.use(express.json());
 app.use("/expense-splits", ExpenseSplitsRouter);
-app.use("/expenses", ExpensesRouter);
+app.use("/expenses", authenticateJWT, ExpensesRouter);
 app.use("/house-members", authenticateJWT, HouseMembersRouter);
 app.use("/houses", authenticateJWT, HousesRouter);
-app.use("/receipts", ReceiptsRouter);
-app.use("/recurring-expenses", RecurringExpensesRouter);
 app.use("/users", UserRouter);
 app.use("/user-houses", UserHousesRouter);
 
 app.use((err, req, res, next) => {
-	const info_string = `Error on path: ${req.path} using ${req.method}`;
-	console.log(info_string);
-	writeLog(info_string, { name: "server-log", dir: "./logs/" });
-	console.error(err.code, "\n", err.e);
-	res.status(err.code || 500).send({});
+  const info_string = `Error on path: ${req.path} using ${req.method}`;
+  console.log(info_string);
+  writeLog(info_string, { name: "server-log", dir: "./logs/" });
+  console.error(err.code, "\n", err.e);
+  res.status(err.code || 500).send({});
 });
 
 module.exports = app;
