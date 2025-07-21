b5ccc7aa9cdabd9befddc2b1dc70b6816b548a6f
jowbrook <jowbrook@ucsc.edu>
2025-07-21 00:25:25 -0700

    backend update

--- backend/node/src/features/houses/controller.js
+++ backend/node/src/features/houses/controller.js
@@ -9,8 +9,7 @@ class HousesController {
    * @param {*} next - Express next function
    */
   async createHouses(req, res, next) {
-    const { name } = req.body;
-    const user_id = req.user.user_id;
+    const { name, user_id } = req.body;
     try {
       const result = await query(
         "INSERT INTO houses (name, user_id) VALUES ($1, $2) RETURNING *",
@@ -93,83 +92,30 @@ class HousesController {
    */
   async updateHouses(req, res, next) {
     const house_id = req.params.id;
-
-    // check if current user is head of house
-    const current_user_id = req.user.user_id;
-    try {
-      await assertUserIsHouseHead(house_id, current_user_id);
-    } catch (error) {
-      return res
-        .status(error.status || 500)
-        .json({ error: error.message || "Internal Server Error" });
+    // dynamically build query text and values, empty queries are not allowed
+    const updates = [];
+    const values = [];
+    if (req.body.name) {
+      values.push(req.body.name);
+      updates.push(`name = $${values.length}`);
     }
-
+    if (req.body.new_user_id) {
+      values.push(new_user_id);
+      updates.push(`user_id = $${values.length}`);
+    }
+    // if nothing to update, return error
+    if (updates.length === 0) {
+      return res
+        .status(400)
+        .json({ error: "No valid fields to update", success: false });
+    }
+    values.push(house_id);
+    // build query text
+    const sql_query = `UPDATE houses SET ${updates.join(
+      ", "
+    )}, updated_at = NOW() WHERE house_id = $${values.length} RETURNING *`;
     try {
-      let updateIsAdmin = false;
-      // dynamically build query text and values, empty queries are not allowed
-      const updates = [];
-      const values = [];
-      if (req.body.name) {
-        values.push(req.body.name);
-        updates.push(`name = $${values.length}`);
-      }
-
-      // if switching head of house, ensure new user is in the house, and isnt current head
-      const new_user_id = req.body.user_id;
-      if (new_user_id && new_user_id != current_user_id) {
-        const validHouseMemberResult = await query(
-          "SELECT 1 FROM house_members WHERE user_id = $1 AND house_id = $2",
-          [new_user_id, house_id]
-        );
-        if (validHouseMemberResult.rows.length === 0) {
-          return res.status(400).json({
-            error: `New user with id "${new_user_id}" not found in house.`,
-            success: false,
-          });
-        }
-        // add to query text and values
-        values.push(new_user_id);
-        updates.push(`user_id = $${values.length}`);
-
-        updateIsAdmin = true;
-      }
-
-      // if nothing to update, return error
-      if (updates.length === 0) {
-        return res
-          .status(400)
-          .json({ error: "No valid fields to update", success: false });
-      }
-
-      // add house_id to values so we can update the specified house
-      values.push(house_id);
-      // build query text
-      const sql_query = `UPDATE houses SET ${updates.join(
-        ", "
-      )}, updated_at = NOW() WHERE house_id = $${values.length} RETURNING *`;
-
       const result = await query(sql_query, values);
-
-      // if no result found, return error
-      if (result.rows.length === 0) {
-        return res.status(404).json({
-          error: `House with ID "${house_id}" not found`,
-          success: false,
-        });
-      }
-
-      if (updateIsAdmin) {
-        await query(
-          "UPDATE house_members SET is_admin = false WHERE house_id = $1 AND user_id = $2",
-          [house_id, current_user_id]
-        );
-        await query(
-          "UPDATE house_members SET is_admin = true WHERE house_id = $1 AND user_id = $2",
-          [house_id, new_user_id]
-        );
-      }
-
-      // else, house found with given parameters return it
       return res.status(200).json({ data: result.rows[0], success: true });
     } catch (error) {
       return next(error);
@@ -184,29 +130,17 @@ class HousesController {
    */
   async deleteHouses(req, res, next) {
     const house_id = req.params.id;
-    const user_id = req.user.user_id;
-
     try {
-      // check if user is head of house
-      await assertUserIsHouseHead(house_id, user_id);
-
       // delete house with house_id from database
       const result = await query(
         "DELETE FROM houses WHERE house_id = $1 RETURNING *",
         [house_id]
       );
-      if (result.rows.length === 0) {
-        return res
-          .status(404)
-          .json({ error: `House with ID "${house_id}" not found` });
-      }
       return res
         .status(200)
         .json({ message: "House deleted successfully", success: true });
     } catch (error) {
-      return res
-        .status(error.status || 500)
-        .json({ error: error.message || "Internal Server Error" });
+      return next(error);
     }
   }
 }
--- backend/node/src/features/houses/validator.js
+++ backend/node/src/features/houses/validator.js
@@ -2,27 +2,6 @@ const { body, query, param } = require("express-validator");
 
 const MAX_NAME_LENGTH = 255;
 
-const optionalFieldValidator = (location, field, max_length) =>
-  location(field)
-    .optional()
-    .isString()
-    .withMessage(`If provided, ${field} must be a string`)
-    .trim()
-    .notEmpty()
-    .withMessage(`If provided, ${field} must not be empty`)
-    .isLength({ max: max_length })
-    .withMessage(`${field} must be at most ${max_length} characters`);
-
-const requiredFieldValidator = (location, field, max_length) =>
-  location(field)
-    .isString()
-    .withMessage(`${field} must be a string`)
-    .trim()
-    .notEmpty()
-    .withMessage(`${field} must not be empty`)
-    .isLength({ max: max_length })
-    .withMessage(`${field} must be at most ${max_length} characters`);
-
 const housesValidators = {
   housesId: [
     param("id")
@@ -32,14 +11,21 @@ const housesValidators = {
       .withMessage("House id must be a valid UUID"),
   ],
   housesQuery: [
-    optionalFieldValidator(query, "name", MAX_NAME_LENGTH),
+    body("name")
+      .optional()
+      .isString()
+      .withMessage(`If provided, name must be a string`)
+      .trim()
+      .notEmpty()
+      .withMessage(`If provided, name must not be empty`)
+      .isLength({ max: MAX_NAME_LENGTH })
+      .withMessage(`name must be at most ${MAX_NAME_LENGTH} characters`),
     query("user_id")
       .optional()
       .notEmpty()
       .withMessage("If provided, user_id must not be empty")
       .isUUID()
       .withMessage("User ID must be a valid UUID"),
-
     query("sort_by")
       .optional()
       .isString()
@@ -48,7 +34,6 @@ const housesValidators = {
       .notEmpty()
       .withMessage(`sorty by must not be empty`)
       .isIn(["created_at", "updated_at", "name"]),
-
     query("sort_order")
       .optional()
       .isString()
@@ -59,7 +44,15 @@ const housesValidators = {
       .isIn(["asc", "desc"]),
   ],
   updateHouses: [
-    optionalFieldValidator(body, "name", MAX_NAME_LENGTH),
+    body("name")
+      .optional()
+      .isString()
+      .withMessage(`If provided, name must be a string`)
+      .trim()
+      .notEmpty()
+      .withMessage(`If provided, name must not be empty`)
+      .isLength({ max: MAX_NAME_LENGTH })
+      .withMessage(`name must be at most ${MAX_NAME_LENGTH} characters`),
     body("user_id")
       .optional()
       .notEmpty()
@@ -67,7 +60,21 @@ const housesValidators = {
       .isUUID()
       .withMessage("user_id must be a valid UUID"),
   ],
-  createHouses: [requiredFieldValidator(body, "name", MAX_NAME_LENGTH)],
+  createHouses: [
+    body("name")
+      .isString()
+      .withMessage(`name must be a string`)
+      .trim()
+      .notEmpty()
+      .withMessage(`name must not be empty`)
+      .isLength({ max: MAX_NAME_LENGTH })
+      .withMessage(`name must be at most ${MAX_NAME_LENGTH} characters`),
+    body("user_id")
+      .notEmpty()
+      .withMessage("user id is required")
+      .isUUID()
+      .withMessage("user id must be a valid UUID"),
+  ],
 };
 
 module.exports = {
