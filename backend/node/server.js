63547f662423f2fe0753d4b862de2c9d1939452c
rm89090000 <riddhimundhra05@gmail.com>
2025-07-21 22:05:54 -0700

    Update server.js

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,12 +1,31 @@
-// server.js
+//server.js
 
 require("dotenv").config();
 
 const app = require("./app");
+const http = require("http");
+const { Server: SocketIOServer } = require("socket.io");
 
 const PORT = process.env.SERVER_PORT || 3001;
-const server = app.listen(PORT, () => {
+
+const server = http.createServer(app);
+
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("connected", {message: "Connection is successful" });
+
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 server.on("close", async () => {});
+
+
+module.exports = {app, server, io};
